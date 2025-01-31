0541aa62220065059a221b21c61a7b4acc169812
Prinkle Singharia <146049399+prinkle-singharia@users.noreply.github.com>
2025-01-31 13:36:42 -0500

    Update ResumeNew.js
    updated resume path

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Prinkle Singharia Resume.pdf";
+import pdf from "../../Assets/Prinkle_Singharia_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
